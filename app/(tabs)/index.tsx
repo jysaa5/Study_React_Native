509057019dd8f61b17c7cdbe29f2f7ff58506b2f
jysaa5 <jysaa5@naver.com>
2025-03-23 23:41:08 +0900

    style 적용해보기

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,25 +5,38 @@ export default function HomeScreen() {
   const [text, setText] = useState('');
   return (
     <SafeAreaView>
-      <View style={styles.container}>
-        <Text style={styles.text}>텍스트</Text>
+      <View style={styles.parent}>
+        <View style={styles.container}>
+          <Text style={styles.text}>텍스트</Text>
+        </View>
+        <View style={styles.container2}>
+          <Text style={styles.text}>텍스트</Text>
+          <Text style={styles.text}>텍스트</Text>
+          <Text style={styles.text}>텍스트</Text>
+        </View>
       </View>
-      <TextInput value={text} onChangeText={(value) => setText(value)} style={styles.input} />
-      <Button title="버튼 이름" onPress={()=> console.log("pressed!")} />
     </SafeAreaView>
   );
 }
 
 
 const styles = StyleSheet.create({
+  parent: {
+    flexDirection: 'row',
+    justifyContent: "center",
+    alignItems: "center"
+  },
   container: {
-    backgroundColor: "yellow"
+    backgroundColor: "yellow",
+  },
+  container2: {
+    backgroundColor: "blue",
   },
   text: {
     color: "red",
-    fontSize: 30
+    fontSize: 30,
   },
   input: {
-    fontSize: 30
-  }
-})
+    fontSize: 30,
+  },
+});
