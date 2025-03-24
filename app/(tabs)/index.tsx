79f4f5c0176c4c18a74e4d7401d8199312bb9fd4
jysaa5 <jysaa5@naver.com>
2025-03-25 02:12:16 +0900

    로그인, 회원가입 마크업

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
 import CustomButton from '@/components/CustomButton';
 import { Text, SafeAreaView } from "react-native";
+import {router} from "expo-router"
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
       <Text>홈스크린</Text>
-      <CustomButton label='버튼' onPress={() => {}}/>
+      <CustomButton label='버튼' onPress={() => router.push('../auth')}/>
     </SafeAreaView>
   );
 }
