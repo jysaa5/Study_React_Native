c778d31d7ee0e22fb9e6233e93ada32b49467a70
jysaa5 <jysaa5@naver.com>
2025-04-06 01:28:21 +0900

    피드 마크업

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,26 @@ import FeedItem from '@/components/FeedItem';
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <FeedItem />
+      <FeedItem
+        post={{
+          id: 1,
+          userId: 1,
+          title: '더미 제목입니다.',
+          description: '더미 내용입니다.',
+          createdAt: '',
+          author: {
+            id: 1,
+            nickname: '닉네임',
+            imageUri: '',
+          },
+          imageUris: [],
+          likes: [],
+          hasVote: false,
+          voteCount: 1,
+          commentCount: 1,
+          viewCount: 1,
+        }}
+      />
     </SafeAreaView>
   );
 }
--- components/FeedItem.tsx
+++ components/FeedItem.tsx
@@ -1,17 +1,46 @@
 import { colors } from '@/constants';
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { Octicons, MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
+import { Post } from '@/types';
 
-interface FeedItemProps {}
+interface FeedItemProps {
+  post: Post;
+}
 
-function FeedItem({}: FeedItemProps) {
+function FeedItem({ post }: FeedItemProps) {
+  const isLiked = true;
+
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
-        <Text>게시글 제목</Text>
-        <Text>게시글 내용</Text>
+        <Text style={styles.title}>{post.title}</Text>
+        <Text numberOfLines={3} style={styles.description}>
+          {post.description}
+        </Text>
       </View>
-      <View style={styles.menuContainer}></View>
+      <View style={styles.menuContainer}>
+        <Pressable style={styles.menu}>
+          <Octicons
+            name={isLiked ? 'heart-fill' : 'heart'}
+            size={16}
+            color={isLiked ? colors.ORANGE_600 : colors.BLACK}
+          />
+          <Text style={isLiked ? styles.activeMenuText : styles.menu}>1</Text>
+        </Pressable>
+        <Pressable style={styles.menu}>
+          <MaterialCommunityIcons
+            name="comment-processing-outline"
+            size={16}
+            color={colors.BLACK}
+          />
+          <Text style={styles.menuText}>1</Text>
+        </Pressable>
+        <Pressable style={styles.menu}>
+          <Ionicons name="eye-outline" size={16} color={colors.BLACK} />
+          <Text style={styles.menuText}>1</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -23,7 +52,39 @@ const styles = StyleSheet.create({
   contentContainer: {
     padding: 16,
   },
-  menuContainer: {},
+  title: {
+    fontSize: 18,
+    color: colors.BLACK,
+    fontWeight: '600',
+    marginVertical: 8,
+  },
+  description: {
+    fontSize: 16,
+    color: colors.BLACK,
+    marginBottom: 14,
+  },
+  menuContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    borderTopColor: colors.GRAY_300,
+  },
+  menu: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 16,
+    width: '33%',
+    gap: 4,
+  },
+  menuText: {
+    fontSize: 14,
+    color: colors.GRAY_700,
+  },
+  activeMenuText: {
+    fontWeight: '500',
+    color: colors.ORANGE_600,
+  },
 });
 
 export default FeedItem;
