3d7cc2840a7f582513f4c966ec061661f8977a88
jysaa5 <jysaa5@naver.com>
2025-04-07 02:22:39 +0900

    회원가입 핸들링 추가

--- app/(tabs)/my/index.tsx
+++ app/(tabs)/my/index.tsx
@@ -1,6 +1,10 @@
+import { useFocusEffect, router } from 'expo-router';
 import { Text, SafeAreaView } from 'react-native';
 
 export default function MyScreen() {
+  useFocusEffect(() => {
+    router.replace('/auth');
+  });
   return (
     <SafeAreaView>
       <Text>내 정보 스크린</Text>
